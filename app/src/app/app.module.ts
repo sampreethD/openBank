32ad6c40e220a0c5cdb505b3ce19b3bdb88e75f4
sampreeth <sampreeth.d@neutrinos.co>
2021-01-31 12:47:22 +0530

    static page

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
